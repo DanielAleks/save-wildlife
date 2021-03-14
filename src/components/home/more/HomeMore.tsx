2132b75d574dec0c64cc64fd656b150fde019444
danielaleksandrov <crunchyhotline@gmail.com>
2021-03-14 03:01:09 -0700

    'home-finished-desktop'

--- src/components/home/more/HomeMore.tsx
+++ src/components/home/more/HomeMore.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './more.sass'
-import { BsArrowDown, BsArrowUp } from 'react-icons/bs'
 
 function HomeMore() {
   return (
@@ -9,6 +8,11 @@ function HomeMore() {
       <div className='more-overlay'>
         <div className='more-bg' />
 
+        <div className='desktop-left-container' >
+          <div className='desktop-left-bg' />
+          <p>Help Me.</p>
+        </div>
+
         <div className='more-content'>
           <header>
             <h1>Save</h1>
@@ -16,11 +20,7 @@ function HomeMore() {
           </header>
           <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt molliti. </p>
           <button className='more-button'>See More...</button>
-          
-          <div className='arrow-box'>
-            <BsArrowDown size={30}/> 
-            <BsArrowUp size={30}/>
-          </div> 
+
         </div>
       </div>
     </div>
