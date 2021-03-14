c8b74d454234ba116baf63df523c18499bdc726d
danielaleksandrov <crunchyhotline@gmail.com>
2021-03-14 04:34:54 -0700

    'gallery-layout-complete'

--- src/components/list-animals/ListAnimals.tsx
+++ src/components/list-animals/ListAnimals.tsx
@@ -4,7 +4,7 @@ import './list-animals.sass'
 import Orangutan from '../../assets/images/orangutan.jpg'
 import Gorilla from '../../assets/images/gorilla.jpg'
 import Bengal from '../../assets/images/bengalTiger.jpg'
-import Elaphant from '../../assets/images/elephant.jpg'
+import Elephant from '../../assets/images/elephant.jpg'
 import Panda from '../../assets/images/panda.jpg'
 import Penguin from '../../assets/images/penguin.jpg'
 import PolarBear from '../../assets/images/polarBear.jpg'
@@ -13,25 +13,37 @@ import Rhino from '../../assets/images/rhino.jpg'
 import Leopard from '../../assets/images/snowLeopard.jpg'
 import Turtle from '../../assets/images/turtle.jpg'
 import Whale from '../../assets/images/whale.jpg'
+import { animalsApi } from '../../ApiInfo'
 
 function ListAnimals() {
 
   const images = [
-    { 1: Orangutan, 2: Gorilla, 3: Bengal },
-    { 1: Elaphant, 2: Panda, 3: Penguin },
-    { 1: PolarBear, 2: RedPanda, 3: Rhino },
-    { 1: Leopard, 2: Turtle, 3: Whale }
+    { image: Bengal, name: 'Bengal Tiger' }, { image: Panda, name: 'Panda' }, { image: Whale, name: 'Blue Whale' },
+    { image: Elephant, name: 'Elephant' }, { image: Gorilla, name: 'Gorilla' }, { image: Leopard, name: 'Snow Leopard' },
+    { image: Orangutan, name: 'Orangutan' }, { image: Turtle, name: 'Sea Turtle' }, { image: Rhino, name: 'Rhinocerus' },
+    { image: Penguin, name: 'Penguin' }, { image: RedPanda, name: 'Red Panda' }, { image: PolarBear, name: 'Polar Bear' }
   ]
 
   return (
     <div className='list-bg'>
 
+      <div className='animal-top-container'>
+        <img className='animal-top-image' src={images[0].image} alt="image" />
+        <div>
+          <p>Information</p>
+          <p>{animalsApi[0].Name}</p>
+          <p>{animalsApi[0].Location}</p>
+          <p>{animalsApi[0].Population}</p>
+          <p>{animalsApi[0].Status}</p>
+          <p>{animalsApi[0].Description}</p>
+        </div>
+      </div>
+
       <div className='list-container'>
         {images.map((Item) =>
           <div className='image-container'>
-            <img src={Item[1]} alt="" />
-            <img src={Item[2]} alt="" />
-            <img src={Item[3]} alt="" />
+              <img src={Item.image} alt="" />
+              <p>{Item.name}</p>
           </div>
         )}
       </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './navbar.sass'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import Modal from './modal/Modal';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 interface isOpen {
   isModalOpen: boolean
@@ -21,12 +26,24 @@ class Navbar extends React.Component<any, isOpen> {
   render() {
     return (
       <div className='nav-container nav-bg'>
-        <p className='help-me'>Help Me.</p>
+        {/* <p className='help-me'>Help Me.</p>
         <GiHamburgerMenu size={25} className='sandwich-icon' onClick={() => this.setState((state) => ({
           isModalOpen: !state.isModalOpen
         }))} />
 
-        {this.state.isModalOpen && <Modal />}
+        {this.state.isModalOpen && <Modal />} */}
+
+        <nav className='nav-container'>
+          <div>
+            <Link style={{ color: '#b56803', borderBottom: '.15rem solid #b56803' }} className='link-d' to="/">Home</Link>
+          </div>
+          <div>
+            <Link className='link-d' to="/gallery">Animals</Link>
+          </div>
+          <div>
+            <Link className='link-d' to="/articles">Articles</Link>
+          </div>
+        </nav>
 
       </div>
     )
