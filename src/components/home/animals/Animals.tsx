b792de663bbb5bf69b1e13af8dee7e12a212bac7
danielaleksandrov <crunchyhotline@gmail.com>
2021-03-15 23:24:24 -0700

    'quick-save'

--- src/components/home/animals/Animals.tsx
+++ src/components/home/animals/Animals.tsx
@@ -2,23 +2,25 @@ import React from 'react'
 import { useSpring, animated } from 'react-spring'
 import './parallax.sass'
 import './animals.sass'
+import Gorilla from '../../../assets/images/gorilla.jpg'
+import Panda from '../../../assets/images/panda.jpg'
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
 const trans1: any = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
 const trans2: any = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`
 const trans3: any = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`
 const trans4: any = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`
-
+ 
 function Animals() {
   const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
   return (
 
     <div id='animals' className='animals-container'>
-      {/* <div className='image-slider-mobile'>
+      <div className='image-slider-mobile'>
         <img className='image1M' src={Gorilla} />
-        <img className='image2M' src={Gorilla} />
-      </div> */}
-
+        <img className='image2M' src={Panda} />
+      </div>
+ 
       <div className='image-slider-desktop'>
         <div className="container" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
           <animated.div className="card1" style={{ transform: props.xy.interpolate(trans1) }} />
--- src/components/home/more/HomeMore.tsx
+++ src/components/home/more/HomeMore.tsx
@@ -25,15 +25,17 @@ class HomeMore extends React.Component<{}, myState> {
               <h1>Save</h1>
               <h1>Wildlife</h1>
             </header>
-            <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt molliti. </p>
-            <Link to="animals" spy={true} smooth={true} offset={0} duration={1500}>
-              <button className='more-button'>See More...</button>
-            </Link>
+            <div className='more-info'>
+              <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt molliti. </p>
+              <Link to="animals" spy={true} smooth={true} offset={0} duration={1500}>
+                <button className='more-button'>See More...</button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div> 
+      </div>
     )
   }
 }
 
-export default HomeMore 
+export default HomeMore
